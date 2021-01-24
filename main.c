c2b58208c0ecb4edfd1bb4654f53b9d74dbfbc8b
AndreyShchur <andrey.shch.97@gmail.com>
2021-01-24 19:21:53 +0300

    Add main code. Blinking led per 2000000 ticks.

--- main.c
+++ main.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
 #include "stm32f10x.h"
 
+#define DoNothing 
+
 int main()
 {
+	RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
+	GPIOC->CRH |= GPIO_CRH_MODE13;
+	GPIOC->CRH &= ~GPIO_CRH_CNF13;
+	long counter = 0;
+	while (1)
+	{
+		GPIOC->BSRR ^= GPIO_BSRR_BS13;
+		while (counter <= 2000000)
+		{
+			counter++;
+		}
+		counter = 0;
+		GPIOC->BSRR ^= GPIO_BSRR_BR13;
+		while (counter <= 2000000)
+		{
+			counter++;
+		}
+		counter = 0;
+	}
 		return 0;
 }
